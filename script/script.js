749efa5e4888c916b15d857d043b14e48b612910
Akhil Chandran <akhil9216@gmail.com>
2020-05-07 00:17:25 +0100

    Experience added

--- script/script.js
+++ script/script.js
@@ -1,35 +1,54 @@
-// Ecperiance Tab ---------->>>
+// Project Tab ---------->>>
 
-function openExperience(evt, experienceName) {
+function openProject(evt, projectName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+        tabcontent[i].style.display = "none";
     }
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    document.getElementById(experienceName).style.display = "block";
+    document.getElementById(projectName).style.display = "block";
     evt.currentTarget.className += " active";
-  }
-    // Get the element with id="defaultOpen" and click on it
-    document.getElementById("defaultOpen").click();
+}
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("projectDefaultOpen").click();
 
 
-// Experiance Contents ------------>>>
+// Project Contents ------------>>>
 
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
 }
+
+// Experiance -------------->>>
+
+function openExperience(companyName, elmnt, color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("ExpTabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(companyName).style.display = "block";
+    elmnt.style.backgroundColor = color;
+
+}
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("ExpDefaultOpen").click();
