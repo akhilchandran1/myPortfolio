f15c7ed1d3b32a4e88fffe749ccc660638e0591f
Akhil Chandran <akhil9216@gmail.com>
2020-05-07 06:11:33 +0100

    Update
    add responsive and add more skills

--- script/script.js
+++ script/script.js
@@ -52,3 +52,15 @@ function openExperience(companyName, elmnt, color) {
 }
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("ExpDefaultOpen").click();
+
+
+// Responsive Navigation Bar --------------->>>
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
